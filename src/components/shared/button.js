ec09ed89139278c76273b876bd21adec2a183898
Jeremy Price <jrp114@yahoo.com>
2023-09-08 20:05:43 -0400

    updated button component layout, added size

--- src/components/shared/button.js
+++ src/components/shared/button.js
@@ -2,11 +2,15 @@ export function Button({
   type = 'button',
   onClick,
   variant = 'primary',
+  size = 'md',
   children,
 }) {
-  let classes = 'text-white p-1 rounded-md';
+  let classes = 'text-white p-1 rounded-md flex items-center justify-center';
   if (variant === 'primary') classes += ' bg-primary';
   else if (variant === 'secondary') classes += ' bg-secondary';
+  if (size === 'sm') classes += ' text-[12px] h-6';
+  else if (size === 'lg') classes += ' text-lg';
+  else classes += ' text-base';
   return (
     <button className={classes} type={type} onClick={onClick}>
       {children}
