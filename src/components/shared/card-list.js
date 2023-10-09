1baf92b8d1f675806912eab8e4b6ba49786003ef
Jeremy Price <jrp114@yahoo.com>
2023-10-09 10:20:24 -0400

    placeholder for new place to add todos

--- src/components/shared/card-list.js
+++ src/components/shared/card-list.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import CardItem from './card-item';
 
 export default function CardList({
@@ -11,6 +11,22 @@ export default function CardList({
 }) {
   const [dragging, setDragging] = useState(false);
   const [index, setIndex] = useState(undefined);
+  const [addTodo, setAddTodo] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    // we want to close the new todo if we escape or click outside
+    const handleClose = (e) => {
+      if (
+        (ref.current && !ref.current.contains(e.target)) ||
+        e.key === 'Escape'
+      ) {
+        setAddTodo(false);
+      }
+    };
+    document.addEventListener('mousedown', handleClose);
+    document.addEventListener('keydown', handleClose);
+  }, [ref]);
 
   return (
     <div className="p-5">
@@ -24,23 +40,41 @@ export default function CardList({
           setCurrent(undefined);
         }}
       >
-        <div className="text-xl text-orange-700 font-bold uppercase">
-          {name}
+        <div className="bg-gray-100 rounded-lg p-3">
+          <div className="text-xl text-orange-700 font-bold uppercase">
+            {name}
+          </div>
+          <div className="flex flex-col gap-2 pt-2">
+            {items?.map((item, i) => (
+              <CardItem
+                key={item.id}
+                item={item}
+                i={i}
+                dragging={dragging}
+                setDragging={setDragging}
+                setIndex={setIndex}
+                items={items}
+                remove={remove}
+                index={index}
+                setCurrent={setCurrent}
+              />
+            ))}
+          </div>
+          {name === 'todo' && !addTodo && (
+            <div
+              className="p-2 cursor-pointer"
+              onClick={() => setAddTodo(true)}
+            >
+              Add something todo
+            </div>
+          )}
+          {addTodo && (
+            <div
+              ref={ref}
+              className="border shadow-md border-gray-300 rounded-lg min-h-[60px] min-w-[150px] p-3 mt-4 bg-white hover:bg-gray-200 cursor-pointer"
+            ></div>
+          )}
         </div>
-        {items?.map((item, i) => (
-          <CardItem
-            key={item.id}
-            item={item}
-            i={i}
-            dragging={dragging}
-            setDragging={setDragging}
-            setIndex={setIndex}
-            items={items}
-            remove={remove}
-            index={index}
-            setCurrent={setCurrent}
-          />
-        ))}
       </div>
     </div>
   );
