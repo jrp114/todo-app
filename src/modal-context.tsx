c063016057e2fa226f3c647dd17c723deeb4821d
Jeremy Price <jrp114@yahoo.com>
2024-07-18 14:21:24 -0400

    update modal context state handling

--- src/modal-context.tsx
+++ src/modal-context.tsx
@@ -4,14 +4,17 @@ import React, {
   useContext,
   useEffect,
   useMemo,
+  useReducer,
   useRef,
-  useState,
 } from 'react';
-import { Maybe } from 'yup';
 import { useAuthContext } from './auth-context';
 import { Button, useOutsideClick } from './components';
 
 interface ModalContextProps {
+  setModal: (args: SetModalProps) => void;
+}
+
+interface ModalContextProviderProps {
   children: React.ReactNode;
 }
 
@@ -21,45 +24,84 @@ interface Action {
 }
 
 interface SetModalProps {
-  message: React.ReactNode | string;
-  actions: Array<Action>;
+  show?: boolean;
+  header?: string;
+  message?: React.ReactNode | string;
+  actions?: Array<Action>;
   noCancelButton?: boolean;
 }
 
-const ModalContext = createContext<any>(undefined);
+interface ModalState {
+  show: boolean;
+  header: string;
+  message: React.ReactNode | string;
+  actions: Array<Action>;
+  cancel: boolean;
+}
 
-export const ModalProvider = ({ children }: ModalContextProps) => {
-  const [show, setShow] = useState(false);
-  const [message, setMessage] = useState<React.ReactNode | string>('');
-  const [actions, setActions] = useState<Array<Action>>([]);
-  const [cancel, showCancel] = useState<Maybe<Boolean>>(false);
+const initialState: ModalState = {
+  show: false,
+  header: '',
+  message: '',
+  actions: [],
+  cancel: false,
+};
+
+const reducer = (
+  state: ModalState,
+  action: {
+    type: string;
+    value: boolean | string | React.ReactNode | Array<Action>;
+  },
+) => {
+  switch (action.type) {
+    case 'show':
+      return Object.assign({}, state, { show: action.value });
+    case 'header':
+      return Object.assign({}, state, { header: action.value });
+    case 'message':
+      return Object.assign({}, state, { message: action.value });
+    case 'actions':
+      return Object.assign({}, state, { actions: action.value });
+    case 'cancel':
+      return Object.assign({}, state, { cancel: action.value });
+    default:
+      return state;
+  }
+};
+
+const ModalContext = createContext<ModalContextProps>(undefined);
+
+export const ModalProvider = ({ children }: ModalContextProviderProps) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
   const { session } = useAuthContext();
   const outer = useRef<any>();
-  useOutsideClick(outer, () => setShow(false));
+  useOutsideClick(outer, () => dispatch({ type: 'show', value: false }));
 
   useEffect(() => {
     if (!session) {
-      setShow(false);
+      dispatch({ type: 'show', value: false });
     }
   }, [session]);
-  const setModal = useCallback(
-    ({ message, actions, noCancelButton }: SetModalProps) => {
-      setActions(actions);
-      setShow(true);
-      setMessage(message);
-      showCancel(!noCancelButton);
-    },
-    [],
-  );
-  const state = useMemo(() => {
+  const setModal = useCallback((args: SetModalProps) => {
+    if (args.show === false) {
+      dispatch({ type: 'show', value: args.show });
+      return;
+    }
+    dispatch({ type: 'header', value: args.header });
+    dispatch({ type: 'message', value: args.message });
+    dispatch({ type: 'actions', value: args.actions });
+    dispatch({ type: 'cancel', value: !args.noCancelButton });
+    dispatch({ type: 'show', value: true });
+  }, []);
+  const value = useMemo(() => {
     return {
       setModal,
-      setShowModal: setShow,
     };
   }, []);
   return (
-    <ModalContext.Provider value={state}>
-      {show && (
+    <ModalContext.Provider value={value}>
+      {state.show && (
         // full screen
         <div className="fixed inset-0 overflow-y-auto bg-black bg-opacity-50">
           {/* outer layer */}
@@ -69,11 +111,18 @@ export const ModalProvider = ({ children }: ModalContextProps) => {
           >
             {/* container */}
             <div className="bg-white p-6 text-lg">
+              {state.header !== '' ? (
+                <div className="text-xl font-bold uppercase text-orange-700">
+                  {state.header}
+                </div>
+              ) : (
+                ''
+              )}
               {/* main dialog */}
-              {message}
+              {state.message}
               <div className="flex justify-end gap-x-4 pt-4">
                 {/* action buttons */}
-                {actions?.map((action: Action, i: number) => (
+                {state.actions?.map((action: Action, i: number) => (
                   <Button
                     key={`action-${i}`}
                     variant="primary"
@@ -84,8 +133,11 @@ export const ModalProvider = ({ children }: ModalContextProps) => {
                     {action.name}
                   </Button>
                 ))}
-                {cancel && (
-                  <Button variant="secondary" onClick={() => setShow(false)}>
+                {state.cancel && (
+                  <Button
+                    variant="secondary"
+                    onClick={() => dispatch({ type: 'show', value: false })}
+                  >
                     Cancel
                   </Button>
                 )}
